0c58ef122254e142819f57c3afddcd94a71587a7
Danang Widiantoro <danang@fisip.net>
2017-09-23 09:20:18 +0700

    manual filemanager

--- modules/_cpanel/admin/filemanager/_switch.php
+++ modules/_cpanel/admin/filemanager/_switch.php
@@ -1,3 +1,5 @@
 <?php  if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
-redirect(_URL.'user/files/?filemanagerPath=');
+$sys->set_layout('blank');
+?>
+<iframe src="<?php echo _URL.'user/files/?filemanagerPath=';?>" frameBorder="0" width="100%" height="100%" scrolling="auto" allowfullscreen allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>
