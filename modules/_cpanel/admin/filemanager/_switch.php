0561ce9d0dcd034ad148daa680f13cf5f2200939
Danang Widiantoro <danang@fisip.net>
2017-09-23 09:40:56 +0700

    bugfixes

--- modules/_cpanel/admin/filemanager/_switch.php
+++ modules/_cpanel/admin/filemanager/_switch.php
@@ -1,5 +1,11 @@
 <?php  if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
-$sys->set_layout('blank');
 ?>
-<iframe src="<?php echo _URL.'user/files/?filemanagerPath=';?>" frameBorder="0" width="100%" height="100%" scrolling="auto" allowfullscreen allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>
+<html>
+	<head></head>
+	<body style="padding: 0; margin: 0;">
+		<iframe src="<?php echo _URL.'user/files/?filemanagerPath=';?>" frameborder="0" width="100%" height="100%" scrolling="auto" style="padding: 0; margin: 0;" allowfullscreen="" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>
+	</body>
+</html>
+<?php
+die();
